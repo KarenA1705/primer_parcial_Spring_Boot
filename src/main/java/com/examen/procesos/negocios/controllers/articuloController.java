36eff5e6be30291a67707a93de3f2d7efd21ea9f
FABIANR-0 <fabianrincon1407@gmail.com>
2022-10-06 19:41:58 -0500

    articulo controller

--- src/main/java/com/examen/procesos/negocios/controllers/articuloController.java
+++ src/main/java/com/examen/procesos/negocios/controllers/articuloController.java
@@ -1,4 +1,13 @@
 package com.examen.procesos.negocios.controllers;
 
+import com.examen.procesos.negocios.repository.ArticuloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class articuloController {
+
+    @Autowired
+    private ArticuloRepository articuloRepository;
+
 }
