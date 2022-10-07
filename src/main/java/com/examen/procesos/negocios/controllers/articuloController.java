eeab7112d7d4f16666ad8b7a44ac9872b24d720f
FABIANR-0 <fabianrincon1407@gmail.com>
2022-10-06 19:45:19 -0500

    articulo controller

--- src/main/java/com/examen/procesos/negocios/controllers/articuloController.java
+++ src/main/java/com/examen/procesos/negocios/controllers/articuloController.java
@@ -1,13 +1,35 @@
 package com.examen.procesos.negocios.controllers;
 
+import com.examen.procesos.negocios.models.Articulo;
 import com.examen.procesos.negocios.repository.ArticuloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class articuloController {
 
     @Autowired
     private ArticuloRepository articuloRepository;
+
+    @GetMapping("/articulo/codigo/{codigo}")
+    public ResponseEntity getArticuloCodigo(@PathVariable String codigo)
+    {
+        List<Articulo> articulo = articuloRepository.findAllByCodigoArticulo(codigo);
+        if(articulo.isEmpty())
+        {
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity(articulo, HttpStatus.OK);
+
+    }
+
+
+
 
 }
--- src/main/java/com/examen/procesos/negocios/repository/ArticuloRepository.java
+++ src/main/java/com/examen/procesos/negocios/repository/ArticuloRepository.java
@@ -3,7 +3,11 @@ package com.examen.procesos.negocios.repository;
 import com.examen.procesos.negocios.models.Articulo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticuloRepository extends JpaRepository<Articulo, String> {
+
+    List<Articulo> findAllByCodigoArticulo(String codigo);
 
 
 }
