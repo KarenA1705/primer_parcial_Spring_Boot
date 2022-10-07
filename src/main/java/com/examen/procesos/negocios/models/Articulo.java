42768651e12b5ec943b0e42cbc5243e1e9e8cce7
KarenA1705 <kamontanon@ufpso.edu.co>
2022-10-06 19:28:53 -0500

    Se creo la conexion entre los modelos de categorias y articulos

--- src/main/java/com/examen/procesos/negocios/models/Articulo.java
+++ src/main/java/com/examen/procesos/negocios/models/Articulo.java
@@ -1,5 +1,37 @@
 package com.examen.procesos.negocios.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "articulo")
+
 public class Articulo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 20, nullable = false, unique = true)
+    private String codigo;
+    @Column(length = 100, nullable = false)
+    private String nombre;
+    @Column(nullable = false)
+    private Date fecha_registro;
+
+    @ManyToOne
+    @JoinColumn(name = "categorias_id_ctg")
+    private Categoria categoria;
+
+    @Column(nullable = false)
+    private int stock;
+    @Column(nullable = false,scale = 2)
+    private Double precio_venta;
+    @Column(nullable = false,scale = 2)
+    private Double precio_compra;
 
 }
